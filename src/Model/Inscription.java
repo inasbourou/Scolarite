6198a38e02d8959c772a09f1313883910032ef5f
sarahazzouz <99666637+sarahazzouz@users.noreply.github.com>
2022-04-23 13:33:45 +0200

    Update Inscription.java

--- src/Model/Inscription.java
+++ src/Model/Inscription.java
@@ -1,5 +1,15 @@
 package Model;
+import java.sql.Date;
 
 public class Inscription {
-
+    private Date date ; 
+    public Inscription (Date date){
+        this.date = date;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
