7a3539d9b2d79dcab4e49e1b577f0b836e0ab2ab
sarahazzouz <99666637+sarahazzouz@users.noreply.github.com>
2022-04-25 15:08:08 +0200

    Update Personne.java

--- src/Model/Personne.java
+++ src/Model/Personne.java
@@ -1,5 +1,31 @@
 package Model;
 
 public class Personne {
+    private String nom ;
+    private String prenom ; 
+    private String address;
 
+    public String getAddress() {
+        return address;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public Personne ( String s1, String s2, String s3) {
+        this.nom = s1 ; 
+        this.prenom = s2;
+        this.address = s3 ; 
+    }
 }
