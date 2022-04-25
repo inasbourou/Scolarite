2cbc0e6cf0f581cdd5835dc74503e95eac0a5edf
sarahazzouz <99666637+sarahazzouz@users.noreply.github.com>
2022-04-25 15:11:34 +0200

    Update Payement.java

--- src/Model/Payement.java
+++ src/Model/Payement.java
@@ -2,20 +2,43 @@ package Model;
 
 import java.util.*;
 
-public class Payement {
-	int Id_paiement;
-	long Numero_carte;
-	String Nom;
-	int datefin_validite;
-	int CVC;
+//public class Payement {
+	//int Id_paiement;
+	//long Numero_carte;
+	//String Nom;
+	//int datefin_validite;
+	//int CVC;
 
-	public Payement(int id_paiement, long numero_carte, String nom, int datefin_validite, int cVC) {
+	//public Payement(int id_paiement, long numero_carte, String nom, int datefin_validite, int cVC) {
 
-		Id_paiement = id_paiement;
-		Numero_carte = numero_carte;
-		Nom = nom;
-		this.datefin_validite = datefin_validite;
-		CVC = cVC;
-	}
+		//Id_paiement = id_paiement;
+		//Numero_carte = numero_carte;
+		//Nom = nom;
+		//this.datefin_validite = datefin_validite;
+		//CVC = cVC;
+	//}
 
+//}
+
+public class Payment {
+    private String numF ; 
+    private boolean statuPay ; 
+    Payment(String numF , boolean statuPay) {  
+        this.numF =  numF ; 
+        this.statuPay = statuPay ; 
+    }
+    public boolean getStatuPay() {
+        return statuPay;
+    }
+    public String getNumF() {
+        return numF;
+    }
+    public void setNumF(String numF) {
+        this.numF = numF;
+    }
+    public void setStatuPay(boolean statuPay) {
+        this.statuPay = statuPay;
+    }
 }
+
+ 
