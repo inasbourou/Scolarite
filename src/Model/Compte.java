70ff8ea4e54fe52fcf10e8fdf38cf55cd9daa804
sarahazzouz <99666637+sarahazzouz@users.noreply.github.com>
2022-05-03 11:59:28 +0200

    Update Compte.java

--- src/Model/Compte.java
+++ src/Model/Compte.java
@@ -19,3 +19,25 @@ public class Compte {
         this.login = login;
     }
 }
+
+
+
+//Ajouter un enfant à son compte
+
+ public Boolean add_son(Enfant enfant){
+        int idp = this.get_sql_parante();
+        if(this.seconnect() && idp!= -1){
+            int id_e =enfant.get_id();
+            String SQL = "INSERT INTO relation_parent_son( id_parent,id_enfant) "
+                    + "VALUES(?,?)";
+                    Connection cnx = Connection.connecrDb();
+            PreparedStatement statement = cnx
+                    .prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+                statement.setInt(1, idp);
+                statement.setInt(2, id_e);
+                statement.executeUpdate();
+            
+            return true;
+        }
+        return false;
+    }
