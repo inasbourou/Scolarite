be14225ba8dacf991393457a94fe23de28778979
sarahazzouz <99666637+sarahazzouz@users.noreply.github.com>
2022-05-04 19:12:13 +0200

    Update Compte.java
    methode ajout d'un conjoint à son compte

--- src/Model/Compte.java
+++ src/Model/Compte.java
@@ -18,7 +18,7 @@ public class Compte {
     public void setLogin(String login) {
         this.login = login;
     }
-}
+
 
 // ajouter un enfant 
  public Boolean add_son(Enfant enfant){
@@ -38,3 +38,22 @@ public class Compte {
         }
         return false;
     }
+    // ajouter un conjoint
+    public Boolean add_conjoint(Parent parent) {
+        int idp = this.get_sql_parante();
+        if (this.seconnect() && idp != -1) {
+            int id_e = parent.get_id();
+            String SQL = "INSERT INTO relation_parent_son( id_parent,id_enfant) "
+                    + "VALUES(?,?)";
+            Connection cnx = Connection.connecrDb();
+            PreparedStatement statement = cnx
+                    .prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, idp);
+            statement.setInt(2, id_e);
+            statement.executeUpdate();
+
+            return true;
+        }
+        return false;
+    }
+}
